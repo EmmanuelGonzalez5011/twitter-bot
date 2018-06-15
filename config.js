3cea189f74d85fd455b411c16994a8596fc87393
Emmanuel Gonzalez <EmmanuelG5011@Gmail.com>
2018-06-14 21:35:20 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '56xUmQPOPN7gKLDz1Hzlx5X57',
+    consumer_secret: 'AdKz0DdLLiXB03zUf2hA0dH58rJK8t89kNVqQlELRlNbxFZHBe',
+    access_token_key: '1007417124871200768-3aRrjHMAw237FTOFoWcA6cLukncuBH',
+    access_token_secret: 'ms3tkEdWTa7EOEq0VphZafD7dXBxUVnJeNZ8PgrIULlna'
+  }
